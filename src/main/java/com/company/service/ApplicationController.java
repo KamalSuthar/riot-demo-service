828c5f0a625b2151b256dd69cee0c1cc0c7b026d
Tihomir Surdilovic <tsurdilovic@tihomirs-mbp.attlocal.net>
2018-09-23 11:13:19 -0700

    update to add alias resolver

--- src/main/java/com/company/service/ApplicationController.java
+++ src/main/java/com/company/service/ApplicationController.java
@@ -8,12 +8,16 @@ import com.myproject.MatchAverages;
 import org.jbpm.services.api.DeploymentService;
 import org.jbpm.services.api.ProcessService;
 import org.jbpm.services.api.RuntimeDataService;
+import org.kie.server.services.api.KieServer;
+import org.kie.server.springboot.jbpm.ContainerAliasResolver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+;
 
 @Controller
 public class ApplicationController {
@@ -27,8 +31,16 @@ public class ApplicationController {
     @Autowired
     private ProcessService processService;
 
+    @Autowired
+    private KieServer kieServer; // have to autowire this bean in order to use aliasresolver atm
+
+    @Autowired
+    private ContainerAliasResolver aliasResolver;
+
     private MatchAverages matchAverages;
     private SummonerInput summonerInput;
+    private String containerAlias = "riotdemocontainer";
+    private String processId = "src.main.resources.summonermatchesprocess";
 
     @GetMapping("/riotdemo")
     public String summonerForm(Model model) {
@@ -65,12 +77,15 @@ public class ApplicationController {
                           summonerInput.getSummonerName());
         processInputs.put("inSummonerPlatform",
                           summonerInput.getSummonerPlatform());
-        processInputs.put("inNumOfMatches", summonerInput.getNumOfGames());
+        processInputs.put("inNumOfMatches",
+                          summonerInput.getNumOfGames());
         processInputs.put("matchaverages",
                           matchAverages);
-        long processInstanceId = processService.startProcess("business-application-kjar-1_0-SNAPSHOT",
-                                                             "src.main.resources.summonermatchesprocess",
-                                                             processInputs);
 
+        String resolvedContainerId = aliasResolver.latest(containerAlias);
+
+        long processInstanceId = processService.startProcess(resolvedContainerId,
+                                                             processId,
+                                                             processInputs);
     }
 }
